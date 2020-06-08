ed96531742d3f7f5d130277ed47f432a5ec29472
NikolayNyunin <n.nyunin@mail.ru>
2020-06-08 20:30:06 +0300

    Added the main style transfer HTML form

--- NST/views.py
+++ NST/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'NST/index.html')
+    style_images = ['Wassily_Kandinsky.jpg']  # temporary placeholder
+    return render(request, 'NST/index.html', {'style_images': style_images})
